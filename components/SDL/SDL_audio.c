317f16a826baad39f2590c20fc2d97af2a65015a
Jason Kirsons <jkirsons@gmail.com>
2018-11-26 19:59:59 +0100

    error checking

--- components/SDL/SDL_audio.c
+++ components/SDL/SDL_audio.c
@@ -17,7 +17,7 @@ IRAM_ATTR void updateTask(void *arg)
 		  //xSemaphoreTake( xSemaphoreAudio, portMAX_DELAY );
 		  memset(sdl_buffer, 0, SAMPLECOUNT*SAMPLESIZE);
 		  (*as.callback)(NULL, sdl_buffer, SAMPLECOUNT*SAMPLESIZE );
-		  i2s_write(I2S_NUM_0, sdl_buffer, SAMPLECOUNT*SAMPLESIZE*2, &bytesWritten, 500 / portTICK_PERIOD_MS);
+		  ESP_ERROR_CHECK(i2s_write(I2S_NUM_0, sdl_buffer, SAMPLECOUNT*SAMPLESIZE*2, &bytesWritten, 500 / portTICK_PERIOD_MS));
 		  //xSemaphoreGive( xSemaphoreAudio );
 		  //vTaskDelay( 1 );
 	  } else
@@ -32,27 +32,27 @@ int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained)
 	.sample_rate = SAMPLERATE,
 	.bits_per_sample = SAMPLESIZE*8, /* the DAC module will only take the 8bits from MSB */
 	.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-	.communication_format = I2S_COMM_FORMAT_I2S_LSB,
+	.communication_format = I2S_COMM_FORMAT_I2S_MSB,
 	.intr_alloc_flags = 0,
 	.dma_buf_count = 4,
 	.dma_buf_len = 512,
-	.use_apll = false
+	.use_apll = true
 	};
 	static const int i2s_num = I2S_NUM_0; // i2s port number
 
-	i2s_driver_install(i2s_num, &i2s_config, 0, NULL);   //install and start i2s driver
+	ESP_ERROR_CHECK(i2s_driver_install(i2s_num, &i2s_config, 0, NULL));   //install and start i2s driver
 	i2s_pin_config_t pc = {
         .bck_io_num = 26,
         .ws_io_num = 25,
-        .data_out_num = -1,
-        .data_in_num = -1                                                       //Not used
+        .data_out_num = I2S_PIN_NO_CHANGE,
+        .data_in_num = I2S_PIN_NO_CHANGE                                                       //Not used
     };
-	i2s_set_pin(i2s_num, &pc);
+	//i2s_set_pin(i2s_num, &pc);
 
-	//i2s_set_pin(i2s_num, NULL);
-	i2s_set_clk(i2s_num, SAMPLERATE, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO);
-	i2s_set_dac_mode(I2S_DAC_CHANNEL_BOTH_EN);
-	i2s_set_sample_rates(i2s_num, SAMPLERATE); 
+	ESP_ERROR_CHECK(i2s_set_pin(i2s_num, NULL));
+	ESP_ERROR_CHECK(i2s_set_clk(i2s_num, SAMPLERATE, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO));
+	ESP_ERROR_CHECK(i2s_set_dac_mode(I2S_DAC_CHANNEL_BOTH_EN));
+	ESP_ERROR_CHECK(i2s_set_sample_rates(i2s_num, SAMPLERATE)); 
 
 	sdl_buffer = malloc(SAMPLECOUNT * SAMPLESIZE * 2);
 
@@ -91,6 +91,7 @@ int SDL_ConvertAudio(SDL_AudioCVT *cvt)
 {
 	Sint16 *sbuf = (Sint16 *)cvt->buf;
 	Uint8 *ubuf = (Uint8 *)cvt->buf;
+
 	for(int i = cvt->len/2-2; i >= 0; i--)
 	{
 /*
@@ -100,8 +101,13 @@ int SDL_ConvertAudio(SDL_AudioCVT *cvt)
 		ubuf[i*4+3] = sbuf[i]<0 ? ((Sint32)sbuf[i] + (Sint32)0x8000) >> 8 : 0;
 		ubuf[i*4+2] = ubuf[i*4+3];
 */
-		sbuf[i*2] = sbuf[i] > 0 ? sbuf[i*2] : 0;
-		sbuf[i*2+1] = sbuf[i] < 0 ? sbuf[i*2] : 0;
+//		sbuf[i*2] = sbuf[i] > 0 ? sbuf[i*2] : 0;
+//		sbuf[i*2+1] = sbuf[i] < 0 ? sbuf[i*2]*-1 : 0;
+	}
+		for(int i = 0; i < cvt->len/2; i++)
+	{
+		sbuf[i*2] = (i <  cvt->len/4) * 0x8000;
+		sbuf[i*2+1] = 0;
 	}
 	return 0;
 }
