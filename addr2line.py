b43381df2ba658f9289a683bfc2bc0bf5b90c8e3
Jason Kirsons <jason@lightningcharged.com>
2018-11-30 18:33:51 +0100

    change CONFIG_SPIRAM_OCCUPY_HSPI_HOST

--- addr2line.py
+++ addr2line.py
@@ -1,10 +1,10 @@
 import subprocess
 from subprocess import call
 
-#cmd = "~/.platformio/packages/toolchain-xtensa32/bin/xtensa-esp32-elf-addr2line "
-cmd = "C:\\Users\\Jason\\.platformio\\packages\\toolchain-xtensa32\\bin\\xtensa-esp32-elf-addr2line "
-#cmd += "-e ~/Documents/GitHub/Doom/.pioenvs/featheresp32/firmware.elf "
-cmd += "-e C:\\Users\\Jason\\Documents\\PlatformIO\\Projects\\OpenTyrian\\.pioenvs\\featheresp32\\firmware.elf "
+cmd = "~/esp/xtensa-esp32-elf/bin/xtensa-esp32-elf-addr2line "
+#cmd = "C:\\Users\\Jason\\.platformio\\packages\\toolchain-xtensa32\\bin\\xtensa-esp32-elf-addr2line "
+cmd += "-e ~/Documents/GitHub/OpenTyrian/build/OpenTyrian.elf "
+#cmd += "-e C:\\Users\\Jason\\Documents\\PlatformIO\\Projects\\OpenTyrian\\.pioenvs\\featheresp32\\firmware.elf "
 cmd += "-a "
 trace = raw_input("stacktrace: ")
 split = trace.split(' ')
